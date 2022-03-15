b9154e6fb92d8450f57e021a55c90b2a40bc7800
Kenneth Muriuki Kimathi <kennethkimken@gmail.com>
2022-03-15 12:29:03 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,32 +1,36 @@
-<<<<<<< HEAD
-Â#include "main.h"
-/**
- *print_alphabet - prints letter a-z
- *Description: Prints all letters in lowercase followed by a new line
- *
-=======
 #include "main.h"
 /**
- *main-check the code
- *print_alphabet - prints the alphabet in lowercase, followed by a new line.
->>>>>>> 075e96544c8e313674b4e4cd1e75726514a78e8f
- *Return: Always 0.
+
+ *print_alphabet - prints a-z
+
+ *
+
+ *Description: prints the alphabet in lowercase followed by a newline
+
+ *
+
+ *Return:void
+
+ *
+
  */
+
 void print_alphabet(void)
+
 {
-<<<<<<< HEAD
+
 	char ch;
-	for (ch = 'a'; ch <= 'z'; ch++);
+
+
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
 	{
+
 		_putchar(ch);
+
 	}
-=======
-	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++);
-
-		_putchar(letter);
-
->>>>>>> 075e96544c8e313674b4e4cd1e75726514a78e8f
 	_putchar('\n');
+
 }
