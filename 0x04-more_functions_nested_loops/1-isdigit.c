c137afa6636c4cdc3398c52dbbe66435cdd75247
kimken-geek <78930556+kimken-geek@users.noreply.github.com>
2022-03-17 22:43:51 +0300

    task

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,47 +1,22 @@
-<<<<<<< HEAD
 #include "main.h"
-
-=======
-Â#include "main.h"
->>>>>>> 8eebacb9c7671de648661d7655683b4e60d3a484
 /**
-
- *_isupper - checks for uppercase characters
-
+ *_isdigit - checks for a digit
  *
-
+ *Description:0 through 9
+ *
  *@c:input
-
- *Return:1 - uppercase,0 - otherwise
-
  *
-
+ *Return:1 - digit 0 - otherwise
+ *
  */
-
-
-
 int _isdigit(int c)
-
 {
-
 	if (c >= '0' && c <= '9')
-
 	{
-
 		return (1);
-
 	}
-
 	else
-
 	{
-<<<<<<< HEAD
-
 		return (0);
-
-=======
-		return(0);
->>>>>>> 8eebacb9c7671de648661d7655683b4e60d3a484
 	}
-
 }
